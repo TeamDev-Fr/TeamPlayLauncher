3823f0aedaa781323267e9f77685c9fd3890a3bb
TeamDev-Fr <83709711+TeamDev-Fr@users.noreply.github.com>
2021-05-15 14:30:23 +0200

    Update distromanager.js

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -537,7 +537,7 @@ exports.pullRemote = function(){
         return exports.pullLocal()
     }
     return new Promise((resolve, reject) => {
-        const distroURL = 'https://raw.githubusercontent.com/TeamDev-Fr/MinecraftServerModules/main/distribution.json?token=AT6U6DYP47RU6S7CFFCQE73AT67JE'
+        const distroURL = 'https://raw.githubusercontent.com/TeamDev-Fr/MinecraftServerModules/main/distribution.json?token=AT6U6D25Y64L2QPM33ROD5LAT67QI'
         //const distroURL = 'https://gist.githubusercontent.com/dscalzi/53b1ba7a11d26a5c353f9d5ae484b71b/raw/'
         const opts = {
             url: distroURL,
